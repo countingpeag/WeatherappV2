d679b2cf6f39928388002660482a3f478e6dfb43
omar4335@hotmail.com <omar4335@hotmail.com>
2021-02-03 22:34:31 -0600

    pages for router functionality

---
+++ src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const MainPage = () => {
+    return (
+        <div>MainPage</div>
+    )
+}
+
+MainPage.propTypes = {
+
+}
+
+export default MainPage
---
+++ src/pages/WelcomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const WelcomePage = ({}) => {
+    return(
+        <div>WelcomePage</div>
+    )
+}
+
+WelcomePage.propTypes = {
+
+}
+
+export default WelcomePage;
