284d3fc064dfc149ef4f1a20b9b4430e96c1e2e8
omar4335@hotmail.com <omar4335@hotmail.com>
2021-02-03 22:56:30 -0600

    Router functionality (BrowserRouter Swithc, Route)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return(
+        <div>
+            <h2>My App</h2>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        Welcome
+                    </Route>
+                    <Route path="/main">
+                        Main
+                    </Route>
+                    <Route path="/city">
+                        City
+                    </Route>
+                    <Route>
+                        Not Found
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
-export default App;
+export default App; 
