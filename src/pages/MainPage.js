bd4ca20113562fccb50c6eb15d2b7dda46c69823
omar4335@hotmail.com <omar4335@hotmail.com>
2021-02-05 22:05:47 -0600

    useHistory (hooks) to modify the URL without link

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const MainPage = () => {
+    const history = useHistory()
+
+    const onClickHandler = () => {
+        //history push allows change the URL by programming
+        history.push("/city")
+
+    }
+
     return (
-        <div>MainPage</div>
+        <div>
+            <button onClick={onClickHandler}>Go to CityPage</button>
+        </div>
     )
 }
 
