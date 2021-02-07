35eb622fcbe12a86ef527195fb45cdb652f12155
omar4335@hotmail.com <omar4335@hotmail.com>
2021-02-06 19:47:13 -0600

    forecastChart responsive

--- src/components/ForecastChart/ForecastChar.js
+++ src/components/ForecastChart/ForecastChar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 
 const ForecastChart = ({data}) => {
     return (
-        <div>
-            <LineChart height={250} width={700} margin={{top:20, bottom:20, left:5, right:5}} data={data}>
+        <ResponsiveContainer height={250} width={"95%"}>
+            <LineChart margin={{top:20, bottom:20, left:5, right:5}} data={data}>
                 <XAxis dataKey={"dayHour"} />
                 <YAxis />
                 <CartesianGrid />
@@ -14,7 +14,7 @@ const ForecastChart = ({data}) => {
                 <Line type={"monotone"} stroke={"#FF0000"} dataKey={"max"} />    
                 <Line type={"monotone"} stroke={"#0000FF"} dataKey={"min"} />   
             </LineChart>
-        </div>
+        </ResponsiveContainer>
     )
 }
 
