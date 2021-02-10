69039395bcd7a47602960a008f7057a5cc07b5cd
omar4335@hotmail.com <omar4335@hotmail.com>
2021-02-09 22:08:19 -0600

    weather refactoring

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import { IconContext} from 'react-icons'
 import IconState, {validValues} from './../IconState'
 
 const Weather = ({temperature, state}) => {
     return (
-        <div>
+        <Grid container item direction="row" alignItems="center" justify="center" spacing={1}>
             <IconContext.Provider value={{ size: '5em'}}>
                 <IconState state={state}/>
             </IconContext.Provider>
             <Typography variant="h3" >{temperature}</Typography>
-        </div>
+        </Grid>
     );
 }
 
