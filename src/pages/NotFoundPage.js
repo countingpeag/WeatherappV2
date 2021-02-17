20840687161512d0e4dad492090750e9c51ebdd1
omar4335@hotmail.com <omar4335@hotmail.com>
2021-02-16 23:19:54 -0600

    Not found page functionaity and style

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,11 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
+import { Link as RouterLink } from 'react-router-dom'
+import Typography from '@material-ui/core/Typography'
+import { IconContext } from 'react-icons'
+import { WiRain } from 'react-icons/wi'
 
 const NotFoundPage = () => {
     return(
-        <div>
-            <Link to={"/main"}>Go back to main</Link>
-        </div>
+        <Grid container direction="column" justify="center" className="full">
+            <div className="highlight">
+                <Grid item container xs={12} justify="center" alignItems="center">
+                    <Grid item>
+                        <IconContext.Provider value={{size:"6em"}}>
+                            <WiRain />
+                        </IconContext.Provider>
+                    </Grid>
+                    <Grid item container direction="column" justify="center" alignItems="center">
+                        <Typography variant="h4" color="inherit">
+                            404! Not found Page
+                        </Typography>
+                        <Link color="inherit" aria-label="menu" component={RouterLink} to="/main">Go Back to main page</Link>
+                    </Grid>
+                </Grid>
+            </div>
+        </Grid>
     )
 }
 
